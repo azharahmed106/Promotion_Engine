d2f37cb0c8eda80221df20a0ed43743c0d66da78
azharahmed106 <azharahmed106@gmail.com>
2020-07-08 22:52:32 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,11 +12,11 @@ namespace Promotion_Engine
         {
             List<Product> products = new List<Product>();
 
-            Console.WriteLine("total number of order");
+            Console.WriteLine("Total number of order");
             int a = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < a; i++)
             {
-                Console.WriteLine("enter the type of product:A,B,C or D");
+                Console.WriteLine("Enter the type of product: A, B, C or D");
                 string type = Console.ReadLine();
                 Product p = new Product(type);
                 products.Add(p);
